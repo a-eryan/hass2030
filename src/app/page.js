80dc908ba7cc3f76691b1085971154811d098e20
Anthony Eryan <eryananthony@gmail.com>
2026-02-17 14:30:30 -0500

    initial design implementation

--- src/app/page.js
+++ src/app/page.js
@@ -95,11 +95,44 @@ export default function Home() {
   }
   return (
     <>
-      <main>
-        <h1>Welcome to HASS Class of 2030</h1>
-        <p>This website is dedicated to the School of Humanities, Arts and Social Sciences (HASS) class of 2030 at Stevens Institute of Technology.</p>
+      <nav className="bg-stevens-red">
+        <ul className="flex gap-10 p-4 justify-center items-center object-contain">
+          <li>
+            <Image src="/stevens-hass-logo-small.svg" alt="HASS Logo" width={70} height={70} />
+          </li>
+          <li className="ml-auto">
+            <a href="https://www.stevens.edu/hass" target="_blank" rel="noopener noreferrer" className="font-extra font-bold text-white text-2xl">
+              HASS MAIN WEBSITE
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/hassatstevens/" target="_blank" rel="noopener noreferrer" className="font-extra font-bold text-white text-2xl">
+              <Image src="/instagram-hass.svg" alt="Instagram Icon" width={28} height={28}/>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/showcase/hassatstevens/" target="_blank" rel="noopener noreferrer" className="font-extra font-bold text-white text-2xl">
+              <Image src="/linkedin-hass.svg" alt="LinkedIn Icon" width={28} height={28}/>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com/HASSatStevens/" target="_blank" rel="noopener noreferrer" className="font-extra font-bold text-white text-2xl">
+              <Image src="/facebook-hass.svg" alt="Facebook Icon" width={28} height={28} />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.youtube.com/@HASSatStevens" target="_blank" rel="noopener noreferrer" className="font-extra font-bold text-white text-2xl">
+              <Image src="/youtube-hass.svg" alt="YouTube Icon" width={28} height={28} /> {/*rendered width of 28x28 is 28px width by 19.38 px height, giving us a ratio of approx. ratio of 1:0.692 width:height.  */}
+            </a>
+
+          </li>
+        </ul>
+      </nav>
+      <main className="p-4">
+        <h1 className="text-7xl text-center p-2">Welcome HASS Class of 2030!</h1>
+        <p>Show your HASS spirit with a custom frame, ranging from professionalism to creativity. Your data has no data stored or shared with any third parties.</p>
         {error && <p className="text-red-500">{error}</p>}
-<div className="flex ">
+      <div className="flex ">
         {previewUrl && (
           <div>
             <h2>Your Frame Preview:</h2>
@@ -120,7 +153,7 @@ export default function Home() {
         </form>
       </div>
       </main>
-      <footer>
+      <footer className="bg-dark-gray text-light-gray p-4 flex flex-col gap-2 mt-4">
         <p>&copy;  2026 Stevens Institute of Technology</p>
         <a href="https://www.stevens.edu/school-humanities-arts-and-social-sciences" target="_blank" rel="noopener noreferrer">
           Learn more about HASS at Stevens Institute of Technology
