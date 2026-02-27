62c0bdd25abc8e4d30a7710ff8f6e4b88372c3df
Anthony Eryan <eryananthony@gmail.com>
2026-02-26 20:10:31 -0500

    ux improvements on larger screens

--- src/app/page.js
+++ src/app/page.js
@@ -114,7 +114,7 @@ export default function Home() {
     });
   }
   return (
-    <div className="min-h-dvh flex flex-col">
+    <div className="min-h-screen flex flex-col">
       <nav className="bg-stevens-red">
         <ul className="flex gap-10 p-4 justify-center items-center object-contain">
           <li>
@@ -147,38 +147,51 @@ export default function Home() {
           </li>
         </ul>
       </nav>
-      <main className="p-4 flex-1">
-        <h1 className="text-7xl text-center p-2">Welcome HASS Class of 2030!</h1>
-        <p>Show your HASS spirit with a custom frame, ranging from professionalism to creativity. Your photos are not collected or shared with Stevens Institute of Technology or any third parties.</p>
+      {/*flex-1: let main grow and shrink as needed to fill available space between header and footer, with footer at bottom of viewport even if main content is short*/}
+      <main className="flex flex-col flex-1 px-4 mb-4">
+        <h1 className="text-7xl text-center p-2 m-4">Welcome HASS Class of 2030!</h1>
+        <p className="p-2 m-4  text-center">Show your HASS spirit with a custom frame, ranging from professionalism to creativity. Your photos are not collected or shared with Stevens Institute of Technology or any third parties.</p>
+        <div className="flex items-center justify-center">
+          <label htmlFor="image" className="flex p-4 mb-3 h-15 w-49 cursor-pointer border-4 border-stevens-red outline-2 outline-solid outline-stevens-gray [outline-offset:-10px] font-extra font-bold text-xl text-stevens-red items-center justify-center text-center [transition:border-width_200ms_ease-in-out,outline_0s_0s] hover:border-10 hover:outline-0  hover:[transition:border-width_200ms_ease-in-out,outline_0s_200ms]">
+            SELECT YOUR PHOTO
+          </label>
+          </div>         
         {error && <p className="text-red-500">{error}</p>}
       <div className="flex ">
-        {previewUrl && (
-          <div className="bg-stevens-red flex flex-col items-center gap-7 p-4 ">
-            <h2 className="text-5xl text-white">Your Frame Preview:</h2>
-            <img src={previewUrl} alt="Frame Preview" className="rounded-full max-h-96" />
-            {frameRequestInProgress ? ( 
-              <button disabled className="cursor-not-allowed p-3 h-15 w-49 border-4 border-medium-gold outline-2 outline-solid outline-light-gray [outline-offset:-10px] font-extra font-bold text-xl text-white items-center justify-center text-center">Processing...</button>
-            ) : (
-              <a href={previewUrl} download="hass_frame.png" className="flex cursor-pointer p-3 h-15 w-49 border-4 border-medium-gold outline-2 outline-solid outline-light-gray [outline-offset:-10px] font-extra font-bold text-xl text-white items-center justify-center text-center [transition:border-width_200ms_ease-in-out,outline_0s_0s] hover:border-10 hover:outline-0 hover:[transition:border-width_200ms_ease-in-out,outline_0s_200ms]">Download Frame</a>
-            )
-            }
-          </div>
-        )}        
-        <form onChange={handleFrameCreation}>
-          <label htmlFor="image" className="flex p-3 h-15 w-49 cursor-pointer border-4 border-stevens-red outline-2 outline-solid outline-stevens-gray [outline-offset:-10px] font-extra font-bold text-xl text-stevens-red items-center justify-center text-center [transition:border-width_200ms_ease-in-out,outline_0s_0s] hover:border-10 hover:outline-0  hover:[transition:border-width_200ms_ease-in-out,outline_0s_200ms]">SELECT YOUR PHOTO</label>
+          <div className=" flex flex-col items-center justify-center gap-4  ">
+            <div className="bg-stevens-red h-full w-full flex flex-col items-center justify-center gap-4 p-4">
+              <h2 className="text-5xl text-white">Your Frame Preview:</h2>
+              {previewUrl ? (
+              <img src={previewUrl} alt="Frame Preview" className="rounded-full max-h-48 bg-background" />
+              ) : (
+                <div className="w-48 h-48 rounded-full bg-light-gray flex items-center justify-center">
+                  <span className="text-center font-bitter">Your framed photo will appear here</span>
+                </div>
+              )
+              }
+          {/*The text-, bg-, and border- Tailwind utilities work because Tailwind inlines their values at compile time. The outline-color utility apparently doesn't get that treatment.*/}
+              {frameRequestInProgress ? ( 
+                <button disabled className="cursor-not-allowed p-3 h-15 w-49 border-4 border-medium-gold outline-2 outline-solid outline-light-gray [outline-offset:-10px] font-extra font-bold text-xl text-white items-center justify-center text-center">Processing...</button>
+              ) : (
+                <a href={previewUrl} download="hass_frame.png" className="flex cursor-pointer p-3 h-15 w-49 border-4 border-medium-gold outline-2 outline-solid outline-light-gray [outline-offset:-10px] font-extra font-bold text-xl text-white items-center justify-center text-center [transition:border-width_200ms_ease-in-out,outline_0s_0s] hover:border-10 hover:outline-0 hover:[transition:border-width_200ms_ease-in-out,outline_0s_200ms]">
+                DOWNLOAD FRAME</a>
+              )}
+            </div>
+          <form onChange={handleFrameCreation}>
           <input id="image" type = "file" name = "image" accept="image/*" required className="hidden"/>
-          <div className="flex flex-wrap gap-4 mt-4">
+          <div className="flex flex-wrap gap-8 align-center justify-center">
             {framesSrc.map((src, index) => (
               <label key={index} className="cursor-pointer hover:scale-110 transition-transform">
-                <Image src={`${src}.png`} alt={`Frame ${index + 1} Preview`} width={200} height={200} className="rounded-full cursor-pointer" />
+                <Image src={`${src}.png`} alt={`Frame ${index + 1} Preview`} width={220} height={220} className=" 2xl:w-full rounded-full cursor-pointer" />
                 <input type="radio" name="frame" value={src.split('/').pop()} required disabled={frameRequestInProgress} className="hidden" />
               </label>
             ))}
           </div>
         </form>
+        </div>    
       </div>
       </main>
-      <footer className="bg-dark-gray text-light-gray p-4 flex flex-col gap-2 h-full">
+      <footer className="bg-dark-gray text-light-gray p-4 gap-4">
         <p>&copy;  2026 Stevens Institute of Technology</p>
       </footer>
     </div>
